8533afbdd2a800e8da8b5db079e781293748dbdf
AilishKavanagh <ailish.j.kavanagh@gmail.com>
2019-03-19 20:14:07 +0000

    Add Custom Recipe Activity

---
+++ app/src/main/java/com/kithcenette/kitchenette/AddRecipeActivity.kt
@@ -0,0 +1,86 @@
+package com.kithcenette.kitchenette
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_add_recipe.*
+import kotlinx.android.synthetic.main.app_bar_add_recipe.*
+
+class AddRecipeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_recipe)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_cupboard -> {
+                val menuIntent = Intent(this@AddRecipeActivity, MainActivity::class.java)
+                startActivity(menuIntent)
+            }
+            R.id.nav_cookbook -> {
+                val menuIntent = Intent(this@AddRecipeActivity, CookbookActivity::class.java)
+                startActivity(menuIntent)
+            }
+            R.id.nav_shopping -> {
+                val menuIntent = Intent(this@AddRecipeActivity, ShoppingListActivity::class.java)
+                startActivity(menuIntent)
+            }
+            R.id.nav_favourite -> {
+                val menuIntent = Intent(this@AddRecipeActivity, FavouritesActivity::class.java)
+                startActivity(menuIntent)
+            }
+            R.id.nav_barcode -> {
+                val menuIntent = Intent(this@AddRecipeActivity, ScanBarcodeActivity::class.java)
+                startActivity(menuIntent)
+            }
+            R.id.nav_share -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
